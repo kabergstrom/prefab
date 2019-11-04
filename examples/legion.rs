cee1802d72b1a97ebda385975ea1e1d1609c1b26
Karl Bergström <karl.anton.bergstrom@gmail.com>
2019-11-04 23:39:14 +0100

    Demonstrate an implementation for prefab ref

--- examples/legion.rs
+++ examples/legion.rs
@@ -1,7 +1,8 @@
+use atelier_core::asset_uuid;
 use prefab::{ComponentTypeUuid, EntityUuid, Prefab, PrefabUuid, StorageDeserializer};
 use serde::{Deserialize, Deserializer, Serialize};
 use serde_diff::{Apply, SerdeDiff};
-use std::cell::RefCell;
+use std::{cell::RefCell, collections::HashMap};
 use type_uuid::TypeUuid;
 mod prefab_sample;
 
@@ -64,13 +65,37 @@ impl EntityCreator {
     }
 }
 
-struct World {
-    transform: RefCell<Option<Transform>>,
+struct RegisteredComponent {
+    deserialize_fn:
+        fn(&mut dyn erased_serde::Deserializer, &mut legion::world::World, legion::entity::Entity),
+    apply_diff:
+        fn(&mut dyn erased_serde::Deserializer, &mut legion::world::World, legion::entity::Entity),
 }
 
-impl prefab::StorageDeserializer for World {
-    fn begin_entity_object(&self, prefab: &PrefabUuid, entity: &EntityUuid) {}
-    fn end_entity_object(&self, prefab: &PrefabUuid, entity: &EntityUuid) {}
+struct InnerWorld {
+    world: legion::world::World,
+    cmd_buffer: legion::command::CommandBuffer,
+    current_entity: Option<legion::entity::Entity>,
+    entity_map: HashMap<EntityUuid, legion::entity::Entity>,
+    registered_components: HashMap<ComponentTypeUuid, RegisteredComponent>,
+}
+
+struct World {
+    inner: RefCell<InnerWorld>,
+}
+
+impl prefab::StorageDeserializer for &World {
+    fn begin_entity_object(&self, prefab: &PrefabUuid, entity: &EntityUuid) {
+        let mut this = self.inner.borrow_mut();
+        let new_entity = this.world.insert((), vec![()])[0];
+        this.current_entity = Some(new_entity);
+        this.entity_map.insert(*entity, new_entity);
+    }
+    fn end_entity_object(&self, prefab: &PrefabUuid, entity: &EntityUuid) {
+        let mut this = &mut *self.inner.borrow_mut();
+        this.current_entity = None;
+        // this.cmd_buffer.write(&mut this.world);
+    }
     fn deserialize_component<'de, D: Deserializer<'de>>(
         &self,
         prefab: &PrefabUuid,
@@ -79,11 +104,29 @@ impl prefab::StorageDeserializer for World {
         deserializer: D,
     ) -> Result<(), D::Error> {
         println!("deserializing transform");
-        *self.transform.borrow_mut() = Some(<Transform as Deserialize>::deserialize(deserializer)?);
-        println!("deserialized {:?}", self.transform);
+        let mut this = self.inner.borrow_mut();
+        let registered = this
+            .registered_components
+            .get(component_type)
+            .expect("failed to find component type");
+        let entity = this.current_entity.expect("no current_entity");
+        (registered.deserialize_fn)(
+            &mut erased_serde::Deserializer::erase(deserializer),
+            &mut this.world,
+            entity,
+        );
+        println!("deserialized component");
         Ok(())
     }
-    fn begin_prefab_ref(&self, prefab: &PrefabUuid, target_prefab: &PrefabUuid) {}
+    fn begin_prefab_ref(&self, prefab: &PrefabUuid, target_prefab: &PrefabUuid) {
+        let prefab = PREFABS
+            .iter()
+            .filter(|p| &p.0 == target_prefab)
+            .nth(0)
+            .expect("failed to find prefab");
+        println!("reading prefab {:?}", prefab.0);
+        read_prefab(prefab.1, self);
+    }
     fn end_prefab_ref(&self, prefab: &PrefabUuid, target_prefab: &PrefabUuid) {}
     fn apply_component_diff<'de, D: Deserializer<'de>>(
         &self,
@@ -93,21 +136,78 @@ impl prefab::StorageDeserializer for World {
         component_type: &ComponentTypeUuid,
         deserializer: D,
     ) -> Result<(), D::Error> {
-        let mut transform = self.transform.borrow_mut();
-        let transform = transform.as_mut().expect("diff but value didn't exist");
+        let mut this = self.inner.borrow_mut();
+        let registered = this
+            .registered_components
+            .get(component_type)
+            .expect("failed to find component type");
+        let entity = *this
+            .entity_map
+            .get(entity)
+            .expect("could not find prefab ref entity");
         println!("applying diff");
-        let before = transform.clone();
-        Apply::apply(deserializer, &mut *transform)?;
-        println!("before {:#?} after {:#?}", before, transform);
+        (registered.apply_diff)(
+            &mut erased_serde::Deserializer::erase(deserializer),
+            &mut this.world,
+            entity,
+        );
         Ok(())
     }
 }
 
-fn main() {
-    let mut deserializer =
-        ron::de::Deserializer::from_bytes(prefab_sample::TEXT.as_bytes()).unwrap();
-    let world = World {
-        transform: RefCell::new(None),
-    };
+const PREFABS: [(PrefabUuid, &'static str); 2] = [
+    (
+        asset_uuid!("5fd8256d-db36-4fe2-8211-c7b3446e1927").0,
+        prefab_sample::PREFAB1,
+    ),
+    (
+        asset_uuid!("14dec17f-ae14-40a3-8e44-e487fc423287").0,
+        prefab_sample::PREFAB2,
+    ),
+];
+
+fn read_prefab(text: &str, world: &World) {
+    let mut deserializer = ron::de::Deserializer::from_bytes(text.as_bytes()).unwrap();
+
     prefab::Prefab::deserialize(&mut deserializer, &world).unwrap();
+}
+
+fn main() {
+    let universe = legion::world::Universe::new();
+    let mut cmd_buffer = legion::command::CommandBuffer::default();
+    cmd_buffer.block = Some(universe.allocator.lock().allocate());
+    use std::iter::FromIterator;
+    let world = World {
+        inner: RefCell::new(InnerWorld {
+            world: universe.create_world(),
+            cmd_buffer,
+            current_entity: None,
+            entity_map: HashMap::new(),
+            registered_components: HashMap::from_iter(vec![(
+                Transform::UUID,
+                RegisteredComponent {
+                    deserialize_fn: |d, world, entity| {
+                        let comp = erased_serde::deserialize::<Transform>(d)
+                            .expect("failed to deserialize transform");
+                        println!("deserialized {:#?}", comp);
+                        world.add_component(entity, comp);
+                    },
+                    apply_diff: |d, world, entity| {
+                        let mut comp = world
+                            .get_component_mut::<Transform>(entity)
+                            .expect("expected component data when diffing");
+                        let mut comp: &mut Transform = &mut *comp;
+                        println!("before diff {:#?}", comp);
+                        <serde_diff::Apply<Transform> as serde::de::DeserializeSeed>::deserialize(
+                            serde_diff::Apply::deserializable(comp),
+                            d,
+                        )
+                        .expect("failed to deserialize diff");
+                        println!("after diff {:#?}", comp);
+                    },
+                },
+            )]),
+        }),
+    };
+    read_prefab(PREFABS[0].1, &world);
 }
--- examples/prefab_sample.rs
+++ examples/prefab_sample.rs
@@ -1,6 +1,29 @@
-pub const TEXT: &str = r#"Prefab(
+pub const PREFAB1: &str = r#"Prefab(
     // Prefab AssetUuid
     id: "5fd8256d-db36-4fe2-8211-c7b3446e1927",
+    objects: [
+       // Embed the contents of another prefab in this prefab and override certain values
+       PrefabRef((
+             prefab_id: "14dec17f-ae14-40a3-8e44-e487fc423287",
+             entity_overrides: [
+                 (
+                      entity_id: "62b3dbd1-56a8-469e-a262-41a66321da8b",
+                      // Override values of a component in an entity of the referenced prefab
+                      component_overrides: [
+                          (
+                              component_type: "d4b83227-d3f8-47f5-b026-db615fb41d31",
+                              diff: [ Enter(Field("translation")), Enter(CollectionIndex(1)), Value(5.0) ]
+                          ),
+                      ],
+                 ),
+             ],
+       ))
+    ]
+)"#;
+
+pub const PREFAB2: &str = r#"Prefab(
+    // Prefab AssetUuid
+    id: "14dec17f-ae14-40a3-8e44-e487fc423287",
     objects: [
         // Inline definition of an entity and its components
         Entity(Entity(
@@ -18,21 +41,5 @@ pub const TEXT: &str = r#"Prefab(
                  ),
              ]
         )),
-       // Embed the contents of another prefab in this prefab and override certain values
-       PrefabRef((
-             prefab_id: "14dec17f-ae14-40a3-8e44-e487fc423287",
-             entity_overrides: [
-                 (
-                      entity_id: "030389e7-7ded-4d1a-aca3-d6912b19116c",
-                      // Override values of a component in an entity of the referenced prefab
-                      component_overrides: [
-                          (
-                              component_type: "d4b83227-d3f8-47f5-b026-db615fb41d31",
-                              diff: [ Enter(Field("translation")), Enter(CollectionIndex(1)), Value(5.0) ]
-                          ),
-                      ],
-                 ),
-             ],
-       ))
     ]
 )"#;
